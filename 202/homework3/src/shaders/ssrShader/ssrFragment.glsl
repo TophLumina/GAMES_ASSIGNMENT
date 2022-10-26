2941ede373dd773a52eeb1eedf3c9ca91f04dab9
Zoe <2625792753@qq.com>
2022-10-26 21:02:23 +0800

    Assignment3 SSR fin

--- 202/homework3/src/shaders/ssrShader/ssrFragment.glsl
+++ 202/homework3/src/shaders/ssrShader/ssrFragment.glsl
@@ -122,8 +122,18 @@ vec3 GetGBufferDiffuse(vec2 uv) {
  *
  */
 vec3 EvalDiffuse(vec3 wi, vec3 wo, vec2 uv) {
-  vec3 L = vec3(0.0);
-  return L;
+  // vec3 L = vec3(0.0);
+  // return L;
+
+  vec3 albedo = GetGBufferDiffuse(uv);
+  vec3 normal_world = GetGBufferNormalWorld(uv);
+
+  // material is diffuse means pdf(wo) = 1/2PI<on semi-sphere>
+  float costheta = max(dot(wi, normal_world), 0.0);
+  vec3 irradience = uLightRadiance * costheta;
+  vec3 radience_out = irradience * INV_TWO_PI;
+
+  return radience_out;
 }
 
 /*
@@ -132,21 +142,79 @@ vec3 EvalDiffuse(vec3 wi, vec3 wo, vec2 uv) {
  *
  */
 vec3 EvalDirectionalLight(vec2 uv) {
-  vec3 Le = vec3(0.0);
-  return Le;
+  // vec3 Le = vec3(0.0);
+  // return Le;
+
+  float visibility = GetGBufferuShadow(uv);
+  vec3 view_dir = normalize(uCameraPos - vPosWorld.xyz);
+
+  vec3 BRDF = EvalDiffuse(uLightDir, view_dir, uv);
+  return BRDF * visibility;
 }
 
 bool RayMarch(vec3 ori, vec3 dir, out vec3 hitPos) {
+  // return false;
+
+  vec2 ori_uv = GetScreenCoordinate(ori);
+  vec2 dir_uv = GetScreenCoordinate(dir);
+
+  const int steps_num = 40;
+  float single_step = 8.0 / float(steps_num) / length(dir_uv);
+
+  for (int i = 1; i < steps_num; ++i) {
+    vec3 now = ori + dir * float(i) * single_step;
+    vec2 now_uv = GetScreenCoordinate(now);
+
+    if (now_uv.x > 1.0 || now_uv.x < 0.0 || now_uv.y > 1.0 || now_uv.y < 0.0)
+      return false;
+    
+    float depth = GetDepth(now);
+    float depth_uv = GetGBufferDepth(now_uv);
+
+    if (depth > depth_uv) {
+      hitPos = now;
+      return true;
+    }
+  }
+
   return false;
 }
 
-#define SAMPLE_NUM 1
+#define SAMPLE_NUM 256
 
 void main() {
   float s = InitRand(gl_FragCoord.xy);
 
   vec3 L = vec3(0.0);
-  L = GetGBufferDiffuse(GetScreenCoordinate(vPosWorld.xyz));
+  // L = GetGBufferDiffuse(GetScreenCoordinate(vPosWorld.xyz));
+  vec2 uv = GetScreenCoordinate(vPosWorld.xyz);
+  vec3 L_d = EvalDirectionalLight(uv) * GetGBufferDiffuse(uv);
+
+  vec3 L_i = vec3(0.0);
+  for (int i = 0; i < SAMPLE_NUM; ++i) {
+    float pdf;
+    vec3 sdir_local = SampleHemisphereUniform(s, pdf);
+
+    vec3 normal_world = GetGBufferNormalWorld(uv);
+    vec3 t;
+    vec3 bt;
+    LocalBasis(normal_world, t, bt);
+    t = normalize(t);
+    bt = normalize(bt);
+    mat3 TBN = mat3(t, bt, normal_world);
+    vec3 sdir_world = TBN * sdir_local;
+
+    vec3 intersection;
+    if (RayMarch(vPosWorld.xyz, sdir_world, intersection)) {
+      vec2 uv_int = GetScreenCoordinate(intersection);
+      L_i += EvalDirectionalLight(uv_int) * GetGBufferDiffuse(uv_int) * EvalDiffuse(normalize(intersection - vPosWorld.xyz), normalize(uCameraPos - vPosWorld.xyz), uv) / pdf;
+    }
+  }
+
+  L_i /= float(SAMPLE_NUM);
+
+  L += L_d + L_i;
+
   vec3 color = pow(clamp(L, vec3(0.0), vec3(1.0)), vec3(1.0 / 2.2));
   gl_FragColor = vec4(vec3(color.rgb), 1.0);
 }
